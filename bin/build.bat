009edbd62a18dc3eb2488a09e80d5637e62368bf
Yusuke Izawa <me@yizawa.com>
2024-09-14 17:28:52 +0900

    Add scripts for windows

--- bin/build.bat
+++ bin/build.bat
@@ -5,7 +5,7 @@ set ocamlyacc=.\win64ocaml\bin\ocamlyacc.exe
 set ocamllex=.\win64ocaml\bin\ocamllex.exe
 set ocamlc=.\win64ocaml\bin\ocamlc.exe
 
+%ocamlc% -c .\syntax.ml 
 %ocamlyacc% .\parser.mly
 %ocamllex% .\lexer.mll
-%ocamlc% -c .\*.mli
-%ocamlc% .\parser.ml .\lexer.ml .\calc.ml -o .\calc
+%ocamlc% .\syntax.ml .\parser.mli .\parser.ml .\lexer.ml .\virtual_stack.ml .\pycode.ml .\emit_pyc.ml .\assemble_pyc.mli .\assemble_pyc.ml .\main.ml -o .\while_lang
